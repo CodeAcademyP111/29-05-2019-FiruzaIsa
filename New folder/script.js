7aad0b231bf48bd5aef8a7db200c9f98b5d740e3
FiruzaIsa <49427451+FiruzaIsa@users.noreply.github.com>
2019-05-31 02:16:37 +0400

    Update script.js

--- New folder/script.js
+++ New folder/script.js
@@ -1,71 +1,49 @@
 $(document).ready(function() {
 
     $(".navigation .col-2").click(function(){
-        var img = $(this).data("img");
-        var msg = $(this).data("msg");
-        $(".slider").children().eq(0).attr("src", `large/${img}`)
-$(".slider").children().eq(1).text(msg);
-if($(".active")){
-    $(".active").removeClass("active");
-}
-$(this).children().eq(0).addClass("active");
- 
- 
+        slideImg($(this))
+        activeImg($(this).children().eq(0))
  
     })
 $("#right_i").click(function(){
     var next =$(".active").parent().next();
     if(next.length!=0){
-        var img = next.data("img");
-        var msg = next.data("msg");
-        $(".slider").children().eq(0).attr("src", `large/${img}`)
-        $(".slider").children().eq(1).text(msg);
-        if($(".active")){
-            $(".active").removeClass("active");
-        }
-        next.children().eq(0).addClass("active");
-        
+        slideImg($(next));
+        activeImg( next.children().eq(0))
+      
     }
     else{
-        var img = $(".navigation").children().eq(0).data("img");
-        var msg = $(".navigation").children().eq(1).data("msg");
-        $(".slider").children().eq(0).attr("src", `large/${img}`)
-        $(".slider").children().eq(1).text(msg);
-        if($(".active")){
-            $(".active").removeClass("active");
-        }
-        $ (".navigation").children().eq(0).children().eq(0).addClass("active");
+        slideImg($(".navigation").children().eq(0))
+        activeImg( $ (".navigation").children().eq(0).children().eq(0))
+       
+     
     }
 })
 
 $("#left_i").click(function(){
-    var next =$(".active").parent().next();
-    if(next.length!=0){
-        var img = next.data("img");
-        var msg = next.data("msg");
-        $(".slider").children().eq(0).attr("src", `large/${img}`)
-        $(".slider").children().eq(1).text(msg);
-        if($(".active")){
-            $(".active").removeClass("active");
-        }
-        next.children().eq(0).addClass("active");
-       
+    var prev =$(".active").parent().prev();
+    if(prev.length!=0){
+        slideImg(prev)
+        activeImg(prev.children().eq(0))     
     }else{ 
-        var img = $(".navigation").children().eq(0).data("img");
-        var msg = $(".navigation").children().eq(1).data("msg");
+        slideImg($(".navigation").children().last())
+        activeImg( $ (".navigation").children().last().children().last())
+        
+    }
+})
+
+function slideImg(elm){
+        var img = elm.data("img");
+        var msg =elm.data("msg");
         $(".slider").children().eq(0).attr("src", `large/${img}`)
         $(".slider").children().eq(1).text(msg);
-        if($(".active")){
-            $(".active").removeClass("active");
-        }
-        $ (".navigation").children().eq(0).children().eq(0).addClass("active");
+}
+
+function activeImg(elm){
+    if($(".active")){
+        $(".active").removeClass("active");
     }
-       
-   
+  elm.addClass("active");
+}
 })
-
-
-})
-
-
 
